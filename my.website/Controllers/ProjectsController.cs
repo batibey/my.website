5f55426625c10c78aeb741e9e156479d343fdaf7
Mustafa BATI <mustafa.bati9@gmail.com>
2024-03-17 14:10:20 +0300

    continue developments

---
+++ my.website/Controllers/ProjectsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using my.website.Entities;
+
+namespace my.website.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly MyWebsiteDbContext _context;
+
+        public ProjectsController(MyWebsiteDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult CreateProjectIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProject(Projects project)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Projects.Add(project);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(); // Eğer model geçersizse, kullanıcıya aynı sayfayı göster
+        }
+
+    }
+}
--- my.website/Entities/Projects.cs
+++ my.website/Entities/Projects.cs
@@ -6,7 +6,7 @@
         public string? ProjectName { get; set; }
         public string? GithubLink { get; set; }
         public string? ProjectDescription { get; set; }
-        public DateTime ProjectDate { get; set; }
+        public DateTime? ProjectDate { get; set; }
         public string? Author { get; set; }
     }
 }
