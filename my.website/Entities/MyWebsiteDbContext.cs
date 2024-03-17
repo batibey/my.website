31d8059ce7cfed912b16989334d4b1e68afd287a
Mustafa BATI <mustafa.bati9@gmail.com>
2024-03-17 11:19:32 +0300

    initial commit

---
+++ my.website/Entities/MyWebsiteDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my.website.Entities
+{
+    public class MyWebsiteDbContext: DbContext
+    {
+        public MyWebsiteDbContext(DbContextOptions<MyWebsiteDbContext> options) : base(options) { }  
+        public DbSet<Projects> Projects { get; set; }
+    }
+}
---
+++ my.website/Entities/Projects.cs
@@ -0,0 +1,12 @@
+﻿namespace my.website.Entities
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string? ProjectName { get; set; }
+        public string? GithubLink { get; set; }
+        public string? ProjectDescription { get; set; }
+        public DateTime ProjectDate { get; set; }
+        public string? Author { get; set; }
+    }
+}
